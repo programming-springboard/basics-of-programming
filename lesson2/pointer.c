b3b82c201e92db65ed3f54e8a908917af75ca494
vitaliy-diachkov <diachkovspecial@gmail.com>
2020-11-19 13:59:31 +0200

    Removed lesson2 folder

--- lesson2/pointer.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a = 1;
-    int *pointerA = &a;
-    printf("Address of variable 'a': %p\n", pointerA);
-    printf("Value of variable 'a': %d\n", *pointerA);
-    return 0;
-}
